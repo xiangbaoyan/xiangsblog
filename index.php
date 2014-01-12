c8be6c0823d2d09e734c8914430a888a2bb9c12c
xiangbaoyan <271190187@qq.com>
2014-01-12 17:04:09 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title></title>
+     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/css/bootstrap.min.css">
+<!--    <link rel="stylesheet" href="/Font-Awesome-3.2.1/css/font-awesome.min.css"/>-->
+      <link rel="stylesheet" href="style/1/basic.css"/>
+    <!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+     <script src="http://cdn.bootcss.com/jquery/1.10.2/jquery.min.js"></script>
+     <script src="http://cdn.bootcss.com/twitter-bootstrap/3.0.3/js/bootstrap.min.js"></script>
+  </head>
+  <body>
+<!--  页头介绍 -->
+    <div class="jumbotron">
+          <div class="container pull-left">
+              <h1>小象网络俱乐部</h1>
+              <span class="label label-default pull-right" >小象网络俱乐部</span>
+          </div>
+
+          <nav class="navbar navbar-default navbar-right">
+             <div class="collapse navbar-collapse" >
+                <ul class="nav navbar-nav">
+                    <li><a href="#">首页</a></li>
+                    <li><a href="#">注册</a></li>
+                    <li><a href="#">登录</a></li>
+                    <li><a href="#">个人中心</a></li>
+                    <li><a href="#">风格</a></li>
+                    <li><a href="#">管理</a></li>
+                    <li><a href="#">退出</a></li>
+                </ul>
+             </div>
+          </nav>
+    </div>
+<!--页中框体-->
+
+<div class="row">
+    <div class="col-md-4">
+        <div class="panel panel-default" id="user">
+            <div class="panel-heading">新进会员</div>
+            <div class="panel-body"></div>
+        </div>
+        <div class="panel panel-default" id="pics">
+            <div class="panel-heading">最新图片</div>
+            <div class="panel-body"></div>
+        </div>
+    </div>
+
+    <div class="col-md-8">
+        <div class="panel panel-default " id="list">
+            <div class="panel-heading">贴子列表</div>
+            <div class="panel-body">
+
+            </div>
+        </div>
+    </div>
+
+</div>
+
+
+
+ </body>
+</html>
